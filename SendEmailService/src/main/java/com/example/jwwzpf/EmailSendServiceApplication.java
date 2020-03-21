cc836b067a1ff9608633027ad17a6f60925a0eb4
Pengfei <zpfeuropa@gmail.com>
2020-03-21 17:33:19 +0100

    push local to remote

---
+++ SendEmailService/src/main/java/com/example/jwwzpf/EmailSendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwwzpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSendServiceApplication.class, args);
+	}
+
+}
---
+++ SendEmailService/src/test/java/com/example/jwwzpf/EmailSendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwwzpf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
